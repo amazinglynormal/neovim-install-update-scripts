1fecea3bcfe16ec5bc4c5b583d1a69ab5ab268d7
Stephen Brady <stephenbrady365@gmail.com>
2025-07-20 20:27:57 +0100

    install-neovim script

---
+++ install-neovim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# Install Dependencies  
+# Fedora/ RHEL -- Uncomment below
+# sudo dnf -y install ninja-build cmake gcc make gettext curl glibc-gconv-extra
+
+# Ubuntu/ Debian -- Uncomment below
+# sudo apt-get install ninja-build gettext cmake curl build-essential
+
+# go to https://github.com/neovim/neovim/blob/master/BUILD.md#build-prerequisites for other systems
+
+
+# Clone neovim repo
+cd ~ && git clone https://github.com/neovim/neovim.git && cd neovim
+
+# If you want to use stable release -- Uncomment below
+# git checkout stable
+
+
+# Install from source
+rm -r build/ # clear the cmake cache
+make CMAKE_EXTRA_FLAGS="-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make install
+
+echo -e "\n\n-------------------------------"
+nvim --version
